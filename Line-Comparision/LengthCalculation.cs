bf9b3de9ab20fa08be132a45ec4754cd559e04bb
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-04 20:03:52 +0530

    [Roshan]Added:Calculate Lenth Of Line

---
+++ Line-Comparision/LengthCalculation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    internal class LengthCalculation
+    {
+        public static void CalculateLength()
+        {
+            int x1, x2, y1, y2;
+            Console.Write("Input an integer value: +x1 +x2 +y1 +y2 ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double distance;
+            distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("distance =" + distance);
+        }
+    }
+}
---
+++ Line-Comparision/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Line_Comparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    LengthCalculation.CalculateLength();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong Choice");
+                    break;
+            }
+        }
+    }
+}
