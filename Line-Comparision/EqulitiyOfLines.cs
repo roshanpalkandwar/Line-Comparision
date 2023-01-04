aa3fea79a8bfacaa4022738d4119526fed6f5908
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-04 20:12:48 +0530

    [Roshan]Added:Calculate Equlatiy Of Line

---
+++ Line-Comparision/EqulitiyOfLines.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    internal class EqulitiyOfLines
+    {
+        public static void Equality()
+        {
+            int x1, x2, y1, y2;
+            Console.Write("Input an integer value: +x1 +x2 +y1 +y2 ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int x4, x5, y4, y5;
+            Console.Write("Input an integer value: +x4 +x5 +y4 +y5 ");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            x5 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+            y5 = Convert.ToInt32(Console.ReadLine());
+
+            double lenght1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double lenght2 = Math.Sqrt(Math.Pow(x4 - x5, 2) + Math.Pow(y4 - y5, 2));
+            Console.WriteLine("length =" + lenght1);
+            Console.WriteLine("length =" + lenght2);
+            Console.WriteLine(lenght1.CompareTo(lenght2));
+        }
+    }
+}
--- Line-Comparision/Program.cs
+++ Line-Comparision/Program.cs
@@ -5,12 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose Option\n 1.Length Of A Line");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     LengthCalculation.CalculateLength();
+                    break;
+                case 2:
+                    EqulitiyOfLines.Equality();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
