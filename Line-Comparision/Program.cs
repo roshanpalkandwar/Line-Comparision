802b0da98b675f41386dac266f1a5e171f806ca3
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-04 20:21:49 +0530

    [Roshan]Added:Compare Length Of Two Line

--- Line-Comparision/Program.cs
+++ Line-Comparision/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines\t 3.CompareLength");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -14,6 +14,9 @@
                     break;
                 case 2:
                     EqulitiyOfLines.Equality();
+                    break;
+                case 3:
+                    CompareLengthOFTwoLines.CompareLength();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
