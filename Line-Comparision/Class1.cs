67442f2549c07dc0b29a8fc12fab02f788b4e322
roshanpalkandwar <roshanpalkandwar@gmail.com>
2023-01-09 21:01:06 +0530

    [Roshan]Added:compareTo methods. - Using Java compareTo method to compare 2 Lengths is

---
+++ Line-Comparision/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    internal class UsingOOPs
+    {
+        private int x1, x2, y1, y2;
+        private int p1, p2, q1, q2;
+        double length1, length2;
+        public void SetLength1(int a1, int a2, int b1, int b2)
+        {
+            x1 = a1;
+            x2 = a2;
+            y1 = b1;
+            y2 = b2;
+        }
+        public void GetLength1()
+        {
+            length1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("length1 : " + length1);
+        }
+
+        public void SetLength2(int m1, int m2, int n1, int n2)
+        {
+            p1 = m1;
+            p2 = m2;
+            q1 = n1;
+            q2 = n2;
+        }
+
+        public void GetLength2()
+        {
+            length2 = Math.Sqrt(Math.Pow((p2 - p1), 2) + Math.Pow((q2 - q1), 2));
+            Console.WriteLine("length2 : " + length2);
+        }
+
+        public void display1()
+        {
+            //Line Comparision using CompareTo Method
+            if (length1.CompareTo(length2) == 0)
+            {
+                Console.WriteLine("The length of two lines are equal");
+            }
+            else if (length1.CompareTo(length2) > 0)
+            {
+                Console.WriteLine("The length of line1 is greater than line2");
+            }
+            else
+            {
+                Console.WriteLine("The length of line1 is less than line2");
+            }
+        }
+
+        public void display2()
+        {
+            //Line Comparision using Equals Method
+            if (length1.Equals(length2))
+            {
+                Console.WriteLine("The length of two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("The length of two lines are not equal");
+            }
+        }
+    }
+}
--- Line-Comparision/Program.cs
+++ Line-Comparision/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation");
-            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines\t 3.CompareLength");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines\t 3.CompareLength\n4.using oops concept");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -20,6 +20,15 @@
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
+                    break;
+                case 4:
+                    UsingOOPs obj = new UsingOOPs();
+                    obj.SetLength1(2, 4, 6, 8);
+                    obj.GetLength1();
+                    obj.SetLength2(2, 4, 6, 8);
+                    obj.GetLength2();
+                    obj.display1();
+                    obj.display2();
                     break;
             }
         }
